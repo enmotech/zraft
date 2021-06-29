dbc548951553722efe945f539d19bf764023f80b
yang.zhang <yang.zhang@enmotech.com>
2021-06-29 15:18:59 +0800

    fix

--- src/replication.c
+++ src/replication.c
@@ -416,8 +416,8 @@ static void assignRoleCb(struct raft_change *req, int status)
 {
 	(void)status;
 	struct assign_result *_result = req->data;
-	struct raft_server *server = configurationGet(&r->configuration, _result->id);
 	struct raft *r = _result->r;
+	struct raft_server *server = configurationGet(&r->configuration, _result->id);
 
 	ZSINFO(gzlog, "[raft][%d][%d][%s]: server[%lld] role:[%d] return.",
 		   rkey(r), r->state, __func__, server->id, server->role);

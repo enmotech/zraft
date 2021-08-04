3a3086075c31f2f367bf16d1f023301d219488ac
yang.zhang <yang.zhang@enmotech.com>
2021-08-04 15:22:37 +0800

    add index for apply.

--- src/replication.c
+++ src/replication.c
@@ -2243,6 +2243,7 @@ static int applyCommand(struct raft *r,
 	request->index = index;
 	request->ab = ab;
 	request->pi = pi;
+	request->req.index = index;
 	request->req.permit = false;
 	request->req.obj_id = bd.obj_id;
 	request->req.chunk_id = bd.chunk_id;

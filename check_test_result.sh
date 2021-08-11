69f57e6654ec25b09c19928801f7babf8f08da6c
root <songlin.tao@enmotech.com>
2021-08-11 10:36:54 +0800

    modify script: add function for make and run tests

--- check_test_result.sh
+++ check_test_result.sh
@@ -1,17 +1,36 @@
 #! /usr/bin/env bash
 
-echo "check raft test result:"
+echo "RESULT:"
 
 log_file='./test-suite.log'
 test_suites=('paper_test' 'etcd_migrate')
 
+#make and run tests
+function make_check(){
+	#clean
+	if [ -f "./Makefile" ];then
+		make clean
+		rm -f Makefile
+	fi
+
+	#configure
+	if [ !-f "./configure" ];then
+		autoreconf -i
+	fi
+	 ./configure --disable-libtool-lock --disable-uv --enable-debug=yes --enable-sanitize=yes --enable-code-coverage
+
+	#make check
+	make check
+}
+
+#check tests result
 function check_test_pass(){
 	test_suite_name=$1
-	if [[ -f "./test-suite.log" ]];then
+	if [ -f "./test-suite.log" ];then
 		test_cnt=`grep $test_suite_name $log_file | wc -l` 
 		pass_cnt=`grep $test_suite_name $log_file | grep 'OK' | wc -l`
 
-		if [[ test_cnt -eq pass_cnt ]];then
+		if [ $test_cnt -eq $pass_cnt ];then
 			echo $test_suite_name: PASS
 			return 0
 		else
@@ -23,18 +42,24 @@ function check_test_pass(){
 	fi
 }
 
-fail_cnt=0
-for test_suite in ${test_suites[@]}; do
-	check_test_pass $test_suite
-	ret=$?
-	fail_cnt=$[$ret+$fail_cnt]
-done
 
-if [[ fail_cnt -eq 0 ]];then
-	exit 0
-else
-	exit 1
-fi
+function work(){
+	make_check
 
+	fail_cnt=0
+	for test_suite in ${test_suites[@]}; do
+		check_test_pass $test_suite
+		ret=$?
+		fail_cnt=$[$ret+$fail_cnt]
+	done
 
+	if [[ fail_cnt -eq 0 ]];then
+		exit 0
+	else
+		exit 1
+	fi
+
+}
+
+work
 

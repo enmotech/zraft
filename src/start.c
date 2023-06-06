e3a259b8dbd9e9ad75c7717009d5eda50d46b4a5
hongwei.wu <hongwei.wu@enmotech.com>
2023-06-06 15:52:30 +0800

    ZSTOR-9402 reset conf index when last_applied >= uncommited conf

--- src/start.c
+++ src/start.c
@@ -411,6 +411,13 @@ static void loadCb(struct raft_io_load *req,
         evtNoticef("raft(%llx) free entry to index %u", r->id, snapshot_index);
     }
 
+    if (r->configuration_uncommitted_index &&
+            r->last_applied >= r->configuration_uncommitted_index) {
+        r->configuration_index = r->configuration_uncommitted_index;
+        r->configuration_uncommitted_index = 0;
+        evtNoticef("raft(%llx) reset conf index %lu", r->configuration_index);
+    }
+
     evtNoticef("raft(%llx) conf start %lu/%lu applied %lu", r->id,
             r->configuration_index, r->configuration_uncommitted_index,
             load->applied_index);

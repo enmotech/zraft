77dcf310b87f80b30d79d25f4b546f832cea8b85
hongwei.wu <hongwei.wu@enmotech.com>
2023-09-26 15:01:10 +0800

    ZSTOR-11358 add limit for log

--- src/event.h
+++ src/event.h
@@ -2,14 +2,31 @@
 #ifndef EVENT_H
 #define EVENT_H
 #include "../include/raft.h"
+#include <time.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define EVT_PER_SEC (200)
 
 const struct raft_event_recorder *eventRecorder(void);
 
-#define evtRecordf(level, fmt, ...)                                        \
-	do {                                                               \
-		eventRecorder()->record(eventRecorder()->data, level,      \
-					__func__, __FILE__, __LINE__, fmt, \
-					__VA_ARGS__);                      \
+#define evtRecordf(level, fmt, ...)                                           \
+	do {                                                                  \
+		bool			 _skip = false;                       \
+		static __thread uint16_t _log_count;                          \
+		static __thread time_t	 _log_time;                           \
+		time_t			 _now = time(NULL);                   \
+		if (_log_time == _now) {                                      \
+			if (++_log_count > EVT_PER_SEC)                       \
+				_skip = true;                                 \
+		} else {                                                      \
+			_log_time  = _now;                                    \
+			_log_count = 1;                                       \
+		}                                                             \
+		if (!_skip)                                                   \
+			eventRecorder()->record(eventRecorder()->data, level, \
+						__func__, __FILE__, __LINE__, \
+						fmt, __VA_ARGS__);            \
 	} while (0)
 
 #define evtErrf(fmt, ...)    evtRecordf(RAFT_ERROR, fmt, __VA_ARGS__)
@@ -21,12 +38,23 @@ const struct raft_event_recorder *eventRecorder(void);
 #define evtIdAllowed(id) \
 	eventRecorder()->is_id_allowed(eventRecorder()->data, id)
 
-#define evtIdRecordf(id, level, fmt, ...)                                      \
-	do {                                                                   \
-		if (eventRecorder()->is_id_allowed(eventRecorder()->data, id)) \
-			eventRecorder()->record(eventRecorder()->data, level,  \
-						__func__, __FILE__, __LINE__,  \
-						fmt, __VA_ARGS__);             \
+#define evtIdRecordf(id, level, fmt, ...)                                     \
+	do {                                                                  \
+		bool			 _skip = false;                       \
+		static __thread uint16_t _log_count;                          \
+		static __thread time_t	 _log_time;                           \
+		time_t			 _now = time(NULL);                   \
+		if (_log_time == _now) {                                      \
+			if (++_log_count > EVT_PER_SEC)                       \
+				_skip = true;                                 \
+		} else {                                                      \
+			_log_time  = _now;                                    \
+			_log_count = 1;                                       \
+		}                                                             \
+		if (!_skip && evtIdAllowed(id))                               \
+			eventRecorder()->record(eventRecorder()->data, level, \
+						__func__, __FILE__, __LINE__, \
+						fmt, __VA_ARGS__);            \
 	} while (0)
 
 #define evtIdErrf(id, fmt, ...)	 evtIdRecordf(id, RAFT_ERROR, fmt, __VA_ARGS__)
